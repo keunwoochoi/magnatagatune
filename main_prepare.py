b79e25499ee46f9d60cc70cdbc4e0faf31341ca7
keunwoochoi <gnuchoi+github@gmail.com>
2016-01-27 12:25:30 +0000

    get sorted_label_matrix

--- main_prepare.py
+++ main_prepare.py
@@ -104,7 +104,7 @@ def create_hdf():
 	for file_write_idx, file_write in enumerate(file_write_ptrs):
 		folder_name = folder_names[file_write_idx]
 		paths_in = [path for path in fm.paths if path[0] == folder_names[set_name_idx]]
-		clip_ids = [clip_id for clip_id in fm.clip_ids if fm.id_to_path[str(clip_id)][0] == folder_name] # [2,6,...
+		clip_ids = [clip_id for clip_id in fm.clip_ids if fm.id_to_paths[str(clip_id)][0] == folder_name] # [2,6,...
 		clip_ids = shuffle(clip_ids)
 		
 		# for data
