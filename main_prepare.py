9dfc8abfd0c3191240084c1f85b067c89a04b2cb
keunwoochoi <gnuchoi+github@gmail.com>
2016-02-10 18:02:21 +0000

    freq band normalisation

--- main_prepare.py
+++ main_prepare.py
@@ -406,13 +406,13 @@ def freq_normalise_dataset(hdf_path):
 	for dataset_name in dataset_names:
 		if dataset_name not in f:
 			continue
-		dataset = f[dataset_name]
-		freq_mean = np.mean(np.mean(np.mean(dataset, axis=0), axis=0), axis=1)
+		# dataset = f[dataset_name]
+		freq_mean = np.mean(np.mean(np.mean(f[dataset_name], axis=0), axis=0), axis=1)
 		freq_mean = freq_mean.reshape((1,1,-1,1))
 
-		freq_var  = np.mean(np.mean(np.var(dataset, axis=0), axis=0), axis=1)
+		freq_var  = np.mean(np.mean(np.var(f[dataset_name], axis=0), axis=0), axis=1)
 		freq_var  = freq_var.reshape((1,1,-1,1))
-		dataset = (dataset - freq_mean) / freq_var
+		f[dataset_name] = (f[dataset_name] - freq_mean) / np.sqrt(freq_var)
 		print '%s, %s: done.' % (hdf_path, dataset_name)
 	return
 
